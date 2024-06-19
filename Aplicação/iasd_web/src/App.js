e9f7a9e5d9b6e0fdac9a926e127dfc9e05ca296a
Giovani Leal Gonçalves <detonsith@gmail.com>
2024-06-18 23:14:19 -0300

    MUI added to project

--- Aplicação/iasd_web/src/App.js
+++ Aplicação/iasd_web/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import CustomizedTables from './Components/componente_de_teste';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <CustomizedTables></CustomizedTables>
         <p>
           Teste acima os componentes que você vai criar em <b>iasd_web/src/Components</b>
         </p>
