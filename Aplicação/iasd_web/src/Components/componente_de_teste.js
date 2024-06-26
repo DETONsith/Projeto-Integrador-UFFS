8241a16218473a1db2cf50ac982aa51427b1f43c
violet657 <vs.3809@gmail.com>
2024-06-26 20:14:23 -0300

    adicionei a tabela do sistema interno da iasd - pessoas

--- Aplicação/iasd_web/src/Components/componente_de_teste.js
+++ Aplicação/iasd_web/src/Components/componente_de_teste.js
@@ -15,12 +15,149 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
+import Collapse from '@mui/material/Collapse';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { jsx } from '@emotion/react';
 
- function newdisabledbutton(text) {
+function newdisabledbutton(text) {
   return (
     <Button variant="contained" disableElevation>
       {text}
     </Button>
+  );
+}
+// tabela para a pagina de pessoas
+function createData(nome, usuario, email, telefone, tipo) {
+  return {
+   nome, 
+   usuario, 
+   email, 
+   telefone, 
+   tipo,
+   history: [
+      {nome: 'Antonio Mauricio dos Santos', usuario: 'antomadosa', email:  'antomadosa@gmail.com',telefone: '(55) 49 99154-3722', tipo: 'Ancião'},
+      {nome: 'Arenato Antunes Barbosa', usuario: 'arenunes.bar', email:  'arenunes316@gmail.com', telefone: '(55) 49 99727-8432', tipo: 'Ancião'},
+      {nome:'Bruno Vieira', usuario: 'brunovi', email: 'brno2276@gmail.com', telefone: '(55) 46 99988-2124', tipo: 'Membro'},
+      {nome: 'Eliza Maria Cristina', usuario: 'macris', email: 'macristina@yahoo.com', telefone: '(55) 49 98822-9748', tipo: 'Membro'},
+      {nome: 'Elizeu Matos Peixoto', usuario: 'elimatos', email: 'peixotoeli@gmail.com', telefone:  '(55) 49 99944-9166', tipo: 'Membro'},
+      {nome: 'Fernando Machado Oliveira', usuario: 'femachado', email: 'femave55112@gmail.com', telefone: '(55) 49 98821-2120', tipo: 'Membro'},
+    ],
+  };
+}
+
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell align="right">{row.usuario}</TableCell>
+        <TableCell align="right">{row.email}</TableCell>
+        <TableCell align="right">{row.telefone}</TableCell>
+        <TableCell align="right">{row.tipo}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ }}>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Nome</TableCell>
+                    <TableCell>Usuário</TableCell>
+                    <TableCell align="right">Email</TableCell>
+                    <TableCell align="right">Telefone</TableCell>
+                    <TableCell align="right">Tipo</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.history.map((historyRow) => (
+                    <TableRow key={historyRow.date}>
+                      <TableCell component="th" scope="row">
+                        {historyRow.date}
+                      </TableCell>
+                      <TableCell>{historyRow.customerId}</TableCell>
+                      <TableCell align="right">{historyRow.amount}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+Row.propTypes = {
+  row: PropTypes.shape({
+    calories: PropTypes.string.isRequired,
+    carbs: PropTypes.string.isRequired,
+    fat: PropTypes.string.isRequired,
+    history: PropTypes.arrayOf(
+      PropTypes.shape({
+        amount: PropTypes.string.isRequired,
+        customerId: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+    nome: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    protein: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
+  createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
+  createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
+  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
+];
+
+function CollapsibleTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>Nome</TableCell>
+            <TableCell align="right">Usuário</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Telefone</TableCell>
+            <TableCell align="right">Tipo</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <Row key={row.name} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
@@ -51,15 +188,15 @@ function ResponsiveDrawer(props) {
       <Toolbar />
       <Divider />
       <List>
-      <ListItem>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <ListItemText>Nome do Usuário</ListItemText>
-        </Grid>
-        <Grid item xs={12}>
-          {newdisabledbutton('Sair')}
-        </Grid>
-      </Grid>
+        <ListItem>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <ListItemText>Nome do Usuário</ListItemText>
+            </Grid>
+            <Grid item xs={12}>
+              {newdisabledbutton('Sair')}
+            </Grid>
+          </Grid>
         </ListItem>
         {['Liturgias', 'Pessoas', 'Visitantes'].map((text, index) => (
           <ListItem key={text} disablePadding>
@@ -73,7 +210,6 @@ function ResponsiveDrawer(props) {
     </div>
   );
 
-  // Remove this const when copying and pasting into your project.
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
@@ -106,7 +242,6 @@ function ResponsiveDrawer(props) {
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
         aria-label="mailbox folders"
       >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Drawer
           container={container}
           variant="temporary"
@@ -114,7 +249,7 @@ function ResponsiveDrawer(props) {
           onTransitionEnd={handleDrawerTransitionEnd}
           onClose={handleDrawerClose}
           ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
+            keepMounted: true,
           }}
           sx={{
             display: { xs: 'block', sm: 'none' },
@@ -139,17 +274,13 @@ function ResponsiveDrawer(props) {
         sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
       >
         <Toolbar />
-
+        <CollapsibleTable />
       </Box>
     </Box>
   );
 }
 
 ResponsiveDrawer.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * Remove this when copying and pasting into your project.
-   */
   window: PropTypes.func,
 };
 
